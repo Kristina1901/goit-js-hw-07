b7061ee53bc3db6c47aac92d7ca29f406a676c85
Kristina <ktuzova1901@gmail.com>
2021-06-28 20:42:11 +0300

    try

---
+++ src/js/task-01.js
@@ -0,0 +1,8 @@
+const menuItemsByClass = document.querySelectorAll(".item");
+console.log(`В списке ${menuItemsByClass.length} категории.`); // ..
+
+const menuItemsByTagName = document.querySelectorAll("h2").forEach((num) => console.log(`Категория:${num.textContent}`));
+
+
+const menuItemsByChildren = document.querySelectorAll("item").forEach((qun) => console.log(qun.length));
+
