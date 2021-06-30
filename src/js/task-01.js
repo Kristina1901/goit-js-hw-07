63d11093291f9f0763b50df3d83c69cad5705540
Kristina <ktuzova1901@gmail.com>
2021-06-30 07:47:21 +0300

    change

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,9 +1,15 @@
 const menuItemsByClass = document.querySelectorAll(".item");
 console.log(`В списке ${menuItemsByClass.length} категории.`); // ..
 
-const menuItemsByTagName = document.querySelectorAll("h2").forEach((num) => console.log(`Категория:${num.textContent}`));
+const dirtyMultiply = (array) => {
+        const title = document.querySelectorAll("h2").forEach(num => console.log(num.textContent));;
+        const items = menuItemsByClass.lastElementChild.forEach(num => console.log(num.length));;
+  }
+;
+
+dirtyMultiply(menuItemsByClass);
 
 
-const menuItemsByChildren = document.querySelectorAll("ul > li");
-console.log( menuItemsByChildren);
+
+
 
