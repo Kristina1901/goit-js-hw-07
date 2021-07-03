892674f91262944a2ea23f1017baf5ef859de155
Kristina <ktuzova1901@gmail.com>
2021-07-03 20:10:38 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,9 +6,15 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
 for (let i = 0; i < ingredients.length; i += 1) {
-  const items = document.createElement('li');
+   const items = document.createElement('li');
+   const list = document.querySelector('ul')
+
+  list.appendChild(items);
   items.textContent = ingredients[i];
-  
-  console.log(items.textContent);
+   console.log(items.textContent);
+ 
 }
+
+
